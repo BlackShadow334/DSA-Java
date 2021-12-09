f6eb94e030e1c367aec02d5d034db13c226504ef
BlackShadow334 <rupeshanand334@gmail.com>
2021-12-09 12:22:04 +0530

    checking for another

--- src/data_structure/Main.java
+++ src/data_structure/Main.java
@@ -2,14 +2,17 @@ package data_structure;
 
 public class Main {
     public static void main(String[] args){
-        StackUsingArray stack = new StackUsingArray(5);
-        stack.push(0);
-        stack.push(1);
-        stack.push(2);
-//        stack.push(3);
-        stack.pop();
-        stack.pop();
+        QueueUsingArray queue = new QueueUsingArray(5);
 
-        System.out.println(stack);
+//        queue.enqueue(0);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(5);
+        System.out.println(queue.isEmpty());
+        System.out.println(queue);
     }
 }
